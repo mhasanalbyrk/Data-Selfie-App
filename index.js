1cde7699ec8890e4e88089f52d71ca12ded097e2
MHA-157 <69457318+MHA-157@users.noreply.github.com>
2021-06-02 23:32:14 +0300

    Port and taking selfie

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const DataStore = require('nedb');
 const app = express();
-app.listen(3000, () => console.log('Listening at 3000'));
+const port = process.env.PORT || 3000;
+// app.listen(3000, () => console.log('Listening at 3000'));
+app.listen(port, () => console.log('Starting server at ${port}'));
 
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
