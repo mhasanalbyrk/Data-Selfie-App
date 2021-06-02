3ea50ad45fa88b227a7a80fb34ae4267d6a1c203
MHA-157 <69457318+MHA-157@users.noreply.github.com>
2021-06-02 22:52:02 +0300

    First push

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const DataStore = require('nedb');
+const app = express();
+app.listen(3000, () => console.log('Listening at 3000'));
+
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new DataStore('database.db');
+database.loadDatabase();
+// database.insert({name: 'Redlag', status: 'logged'});
+
+
+app.get('/api', (req, res) => {
+    database.find({}, (err, data) => {
+        if (err){
+            res.end();
+            return;
+        }
+        res.json(data);
+    });
+
+});
+
+app.post('/api', (request, response) => {
+    console.log('I got request!')
+    console.log(request.body);
+
+    const timestamp = Date.now();
+
+    const data = request.body;
+    data.timestamp = timestamp;
+    database.insert(data);
+
+    response.json(data);
+});
